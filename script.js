740866060c9d96226b0573177b96caa07e2749c2
harshiii <harshitakashyap949@gmail.com>
2026-08-30 23:46:42 +0530

    card flip project

---
+++ script.js
@@ -0,0 +1,18 @@
+function applyTheme(theme) {
+    document.documentElement.setAttribute("data-theme",theme);
+    // console.log(document.documentElement);
+}
+
+function toggleTheme() {
+    const current = document.documentElement.getAttribute("data-theme");
+    console.log(current);
+    if(current === "dark"){
+        applyTheme("light");
+    } else {
+        applyTheme("dark");
+    }
+}
+
+const btn = document.getElementById("toggle");
+
+btn.addEventListener("click" , toggleTheme);
